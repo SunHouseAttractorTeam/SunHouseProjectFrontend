f66b99e8828cbf82cc28e907d7124737a0f9f8f8
SpeedrunDi <amanmaksat0@gmail.com>
2022-11-05 17:58:45 +0600

    SUN-12 Создал about блок и добавил _mixins чтоб вертска не сломалась.

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import About from "../../components/About/About";
 import './Main.scss';
 
 const Main = () => {
     return (
         <div className={'main'}>
             <Header/>
+            <About/>
             <Footer/>
         </div>
     );
