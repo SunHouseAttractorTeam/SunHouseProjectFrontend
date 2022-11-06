ab7ba9aebdcb8de86b347ab0d05c57b07f2bc056
Vitaliy SCHeglov <mlacvital@gmail.com>
2022-11-07 02:17:52 +0600

    SUN-15 Добавил иконку EduSpaceIcon. Сделал блок rates, добавил методы onMouseMove и onMouseOut и функции к ним для изменения классов для hover, на ваниле. Подогнал блок под pixel perfect.

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import Rates from "../../components/Rates/Rates";
 import './Main.scss';
 
 const Main = () => {
     return (
         <div className={'main'}>
             <Header/>
+            <Rates/>
             <Footer/>
         </div>
     );
