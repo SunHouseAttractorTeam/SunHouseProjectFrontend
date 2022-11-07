7d7062c0a74b9ad29b4c2edcd770543903208607
Atomico592 <atomico592@gmail.com>
2022-11-07 09:55:16 +0600

    SUN-17 Добавил фрагмент страницы с формой, добавил к нему стили, добавил _fonts

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import SelectionHelpForm from "../../components/SelectionHelpForm/SelectionHelpForm";
 import './Main.scss';
 
 const Main = () => {
     return (
         <div className={'main'}>
             <Header/>
+            <SelectionHelpForm/>
             <Footer/>
         </div>
     );
