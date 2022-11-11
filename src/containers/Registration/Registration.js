305246189788016a91b56b89266d38735fd6118d
Ilim Alybekov <ilimalybekov2204@gmail.com>
2022-11-11 13:34:17 +0600

    SUN-38 Добавлены стили для формы регистрации

--- src/containers/Registration/Registration.js
+++ src/containers/Registration/Registration.js
@@ -8,6 +8,8 @@ import AppleLogin from '../../components/services/AppleLogin/AppleLogin'
 import VkontakteLogin from '../../components/services/VkontakteLogin/VkontakteLogin'
 import Logo from '../../components/UI/Logo/Logo'
 import FooterLink from '../../components/Footer/FooterLink/FooterLink'
+import VectorImage from '../../assets/images/Vector-2.png'
+import './Registration.scss'
 
 const Registration = () => {
   const dispatch = useDispatch()
@@ -34,29 +36,37 @@ const Registration = () => {
           <Logo className="header_logo" />
         </div>
       </header>
-      <form onSubmit={submitFormHandler}>
-        <FormInput
-          required
-          placeholder="Username"
-          name="username"
-          value={user.username}
-          onChange={inputChangeHandler}
-        />
-        <FormInput required placeholder="Email" name="email" value={user.email} onChange={inputChangeHandler} />
-        <FormInput
-          type="password"
-          required
-          placeholder="Password"
-          name="password"
-          value={user.password}
-          onChange={inputChangeHandler}
-        />
-        <button type="submit">Sign Up</button>
-        <FacebookLogin />
-        <GoogleLogin />
-        <AppleLogin />
-        <VkontakteLogin />
-      </form>
+      <div className={`${'container'} ${'registration'}`}>
+        <form onSubmit={submitFormHandler} className="form">
+          <FormInput
+            required
+            placeholder="Username"
+            name="username"
+            value={user.username}
+            onChange={inputChangeHandler}
+          />
+          <FormInput required placeholder="Email" name="email" value={user.email} onChange={inputChangeHandler} />
+          <FormInput
+            type="password"
+            required
+            placeholder="Password"
+            name="password"
+            value={user.password}
+            onChange={inputChangeHandler}
+          />
+          <button type="submit">Sign Up</button>
+          <FacebookLogin />
+          <GoogleLogin />
+          <AppleLogin />
+          <VkontakteLogin />
+        </form>
+        <div className="register-block">
+          <p className="register-block_content">
+            Авторизуйтесь, чтоб начать <span className="register-block_word">учиться</span>
+          </p>
+          <img src={VectorImage} alt="Vector" />
+        </div>
+      </div>
       <footer className="footer_login">
         <div className="container">
           <div className="footer_bottom_inner">
