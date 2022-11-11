eea2f79ca8ef9698f7d0e0a7f9689360c0115f27
SpeedrunDi <amanmaksat0@gmail.com>
2022-11-11 11:58:32 +0600

    SUN-39 Добавил запросы для courses(actions, sagas, slice).

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import usersSlice from './slices/usersSlices'
+import coursesSlice from './slices/coursesSlice'
 
 const rootReducer = combineReducers({
   users: usersSlice.reducer,
+  courses: coursesSlice.reducer,
 })
 
 export default rootReducer
--- src/store/rootSagas.js
+++ src/store/rootSagas.js
@@ -2,7 +2,8 @@ import { all } from 'redux-saga/effects'
 import userSagas from './sagas/usersSagas'
 import historySagas from './sagas/historySagas'
 import history from '../history'
+import coursesSagas from './sagas/coursesSagas'
 
 export default function* rootSagas() {
-  yield all([...userSagas, ...historySagas(history)])
+  yield all([...userSagas, ...coursesSagas, ...historySagas(history)])
 }
