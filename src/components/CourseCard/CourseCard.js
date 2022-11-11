b69fd2a0cc00a6526d976cb56543273800957f88
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-11-11 18:14:29 +0600

    SUN-14 рефакторинг sun-11,обновили coursesData

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -3,17 +3,17 @@ import React from 'react'
 import './CourseCard.scss'
 import MainButton from '../UI/MainButton/MainButton'
 
-const CourseCard = ({ isLast, title, date }) => (
+const CourseCard = ({ isLast, title, date, list }) => (
   <div className={isLast ? 'course-card course-card--is-last' : 'course-card'}>
     <div className="course-card__content">
       {!isLast && <span className="course-card__profession">профессия</span>}
       <h3 className="course-card__title">{title}</h3>
       {!isLast && date && <span className="course-card__date">{date}</span>}
-      {isLast && (
+      {(isLast && list) && (
         <ul className="course-card__list">
-          <li className="course-card__list-item">Программирование</li>
-          <li className="course-card__list-item">Дизайн и UX</li>
-          <li className="course-card__list-item">Маркетинг</li>
+            {list.map(item => (
+                <li key={item} className="course-card__list-item">{item}</li>
+            ))}
         </ul>
       )}
       {isLast && <MainButton className="course-card__button" text="Подробнее" />}
