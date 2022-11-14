fbbf93e3f1de48b2e5fa227359e964ab8d2c673d
Vitaliy SCHeglov <mlacvital@gmail.com>
2022-11-14 20:10:12 +0600

    DEV- refactor code

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
-import SelectionHelpForm from '../../components/SelectionHelpForm/SelectionHelpForm'
-import Rates from '../../components/Rates/Rates'
 import About from '../../components/About/About'
 import TeachersBlock from '../../components/TeachersBlock/TeachersBlock'
 import Promo from '../../components/Promo/Promo'
@@ -18,8 +16,6 @@ const Main = () => (
     <About />
     <TeachersBlock />
     <Partners />
-    <Rates />
-    <SelectionHelpForm />
     <Footer />
   </div>
 )
