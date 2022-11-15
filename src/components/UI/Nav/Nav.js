1999c25c1ebf9a8bcda0e05e117cf33048ce0368
Vitaliy SCHeglov <mlacvital@gmail.com>
2022-11-15 17:58:41 +0600

    SUN-46 Сделал меню для кнопки Каталог курсов, захардкодил пока временные категории, прошел циклом и добавил в меню. Добавил функцию funcIsOpenMenu в которой через toggle меняю класс на is-open. Так же поправил FooterIcon

--- src/components/UI/Nav/Nav.js
+++ src/components/UI/Nav/Nav.js
@@ -2,36 +2,57 @@ import React from 'react'
 import './Nav.scss'
 import { NavLink } from 'react-router-dom'
 
-const Nav = () => (
-  <nav className="main-nav">
-    <ul className="main-nav__list">
-      <li className="main-nav__item">
-        <a href="#" className="main-nav__link">
-          Каталог курсов
-        </a>
-      </li>
-      <li className="main-nav__item">
-        <a href="#" className="main-nav__link">
-          Бесплатные курсы
-        </a>
-      </li>
-      <li className="main-nav__item">
-        <a href="#" className="main-nav__link">
-          О школе
-        </a>
-      </li>
-      <li className="main-nav__item">
-        <a href="#" className="main-nav__link">
-          Отзывы
-        </a>
-      </li>
-      <li className="main-nav__item">
-        <NavLink to="/registration" className="main-nav__link">
-          Регистрация
-        </NavLink>
-      </li>
-    </ul>
-  </nav>
-)
+const Nav = () => {
+  const catalogs = ['Первый', 'Второй', 'Третий']
 
+  const funcIsOpenMenu = event => {
+    event.preventDefault()
+    const dropdown = event.target.parentNode
+    dropdown.classList.toggle('dropdown__is-open')
+  }
+
+  return (
+    <nav className="main-nav">
+      <ul className="main-nav__list">
+        <div className="dropdown">
+          <button type="button" className="dropdown__toggle" onClick={funcIsOpenMenu}>
+            Каталог курсов
+          </button>
+
+          <div className="dropdown__drawer">
+            <ul className="menu">
+              {catalogs.map((list, index) => (
+                <li key={index}>
+                  <NavLink className="menu__link" to={`/${list}`}>
+                    {list}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <li className="main-nav__item">
+          <a href="#" className="main-nav__link">
+            Бесплатные курсы
+          </a>
+        </li>
+        <li className="main-nav__item">
+          <a href="#" className="main-nav__link">
+            О школе
+          </a>
+        </li>
+        <li className="main-nav__item">
+          <a href="#" className="main-nav__link">
+            Отзывы
+          </a>
+        </li>
+        <li className="main-nav__item">
+          <NavLink to="/registration" className="main-nav__link">
+            Регистрация
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
 export default Nav
