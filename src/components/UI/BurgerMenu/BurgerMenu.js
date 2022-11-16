b52c4715848d2752b5ab2660f5828bdfd24ddb86
Erkin_Suyumbaev <smbv.erkin@gmail.com>
2022-11-16 20:09:43 +0600

    SUN-42 Создал компоненты Title и BurgerMenu, добавил их в "Мои курсы"

---
+++ src/components/UI/BurgerMenu/BurgerMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './BurgerMenu.scss'
+
+const BurgerMenu = () => (
+  <div className="burger-menu">
+    <a href="#" className="burger-menu__button">
+      <span className="burger-menu__lines" />
+    </a>
+  </div>
+)
+export default BurgerMenu
--- src/containers/UserCourses/UserCourses.js
+++ src/containers/UserCourses/UserCourses.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Title from '../../components/UI/Title/Title'
+import BurgerMenu from '../../components/UI/BurgerMenu/BurgerMenu'
+import './UserCourses.scss'
 
 const UserCourses = () => (
   <div className="user-courses">
-    <div className="user-courses_title">Мои курсы</div>
+    <div className="user-courses__top">
+      <Title>Мои курсы</Title>
+      <BurgerMenu />
+    </div>
   </div>
 )
 
