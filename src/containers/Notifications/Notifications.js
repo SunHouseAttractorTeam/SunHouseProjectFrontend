f7ce933f1aaba23080e87796c81360edef06745e
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-11-17 22:17:01 +0600

    SUN-43 сделана верстка блока с уведомлениями

--- src/containers/Notifications/Notifications.js
+++ src/containers/Notifications/Notifications.js
@@ -1,5 +1,18 @@
 import React from 'react'
+import './Notifications.scss'
 
-const Notifications = () => <div>Notifications</div>
+const Notifications = () => (
+  <div className="notifications-card__content">
+    <h2 className="notifications-card__title">Уведомления</h2>
+    <div className="notifications-card__view-status">
+      <button className="notifications-card__view-status__view active">Просмотренные</button>
+      <button className="notifications-card__view-status__unview new-notifications">Непросмотренные</button>
+    </div>
+    <p className="notifications-card__text">
+      Разнообразный и богатый опыт сложившаяся структура организации обеспечивает широкому кругу (специалистов) участие
+      в формировании существенных финансовых и административных условий.
+    </p>
+  </div>
+)
 
 export default Notifications
