f89971fb6229942d3509ac7fa33bc228baaebd15
Erkin_Suyumbaev <smbv.erkin@gmail.com>
2022-11-17 20:02:56 +0600

    SUN-34 Отредактировал классы по правильный БЭМ

--- src/containers/MyProfile/MyProfileSidebarLink/MyProfileSidebarLink.js
+++ src/containers/MyProfile/MyProfileSidebarLink/MyProfileSidebarLink.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const MyProfileSidebarLink = ({ className, children, to }) => (
-  <li className="profile_sidebar_bottom_nav_list_item">
-    <Link className={`profile_sidebar_bottom_nav_list_item_link ${className}`} to={to}>
+  <li className="profile__sidebar-bottom-nav-list-item">
+    <Link className={`profile__sidebar-bottom-nav-list-item-link ${className}`} to={to}>
       {children}
     </Link>
   </li>
--- src/containers/MyProfile/MyProfileTop/MyProfileTop.js
+++ src/containers/MyProfile/MyProfileTop/MyProfileTop.js
@@ -3,15 +3,15 @@ import Logo from '../../../components/UI/Logo/Logo'
 import Avatar from '../../../components/UI/Avatar/Avatar'
 
 const MyProfileTop = ({ user }) => (
-  <div className="profile_sidebar_top">
-    <div className="profile_sidebar_top_logo">
-      <Logo className="profile_logo" />
+  <div className="profile__sidebar-top">
+    <div className="profile__sidebar-top-logo">
+      <Logo className="profile__logo" />
     </div>
-    <div className="profile_sidebar_top_user">
-      <Avatar className="profile_sidebar_top_user_avatar" user={user} />
+    <div className="profile__sidebar-top-user">
+      <Avatar className="profile__sidebar-top-user-avatar" user={user} />
       <div>
-        <p className="profile_sidebar_top_user_username">{user?.username}</p>
-        <p className="profile_sidebar_top_user_role">{user?.role}</p>
+        <p className="profile__sidebar-top-user-username">{user?.username}</p>
+        <p className="profile__sidebar-top-user-role">{user?.role}</p>
       </div>
     </div>
   </div>
