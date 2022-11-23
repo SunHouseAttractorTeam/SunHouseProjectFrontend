1c76eb45a6c337192e485b898881c84d1c70137b
Vitaliy SCHeglov <mlacvital@gmail.com>
2022-11-23 16:17:28 +0600

    SUN-72 Сделал оформление и внешний вид, добавил функции на кнопки.

--- src/containers/TeacherMode/TeacherMode.js
+++ src/containers/TeacherMode/TeacherMode.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchCoursesRequest } from '../../store/actions/coursesActions'
+import Card from '../../components/UI/Cards/Card/Card'
+import Title from '../../components/UI/Title/Title'
+import MainButton from '../../components/UI/MainButton/MainButton'
+import addIcon from '../../assets/icons/addIcon.svg'
+import './TeacherMode.scss'
 
 const TeacherMode = () => {
   const dispatch = useDispatch()
@@ -10,11 +15,25 @@ const TeacherMode = () => {
     dispatch(fetchCoursesRequest())
   }, [dispatch])
 
+  const goToEditCourse = () => {}
+
+  const goToNewCourse = () => {}
+
   return (
-    <>
-      <div>Teacher mode</div>
-      <p>Teacher mode</p>
-    </>
+    <div className="AllCard">
+      {courses &&
+        courses.map(item => (
+          <Card key={item._id} className="WhiteCard AllCard__teacher">
+            <span className="course-card__profession">профессия</span>
+            <Title>{item.title}</Title>
+            <MainButton className="GreenButton" text="Изменить курс" onClick={goToEditCourse} />
+          </Card>
+        ))}
+      <Card className="WhiteCard AllCard__teacher">
+        <MainButton className="WhiteButton" text={<img src={addIcon} alt="add" onClick={goToNewCourse} />} />
+        <span className="course-card__profession">Создать курс</span>
+      </Card>
+    </div>
   )
 }
 
