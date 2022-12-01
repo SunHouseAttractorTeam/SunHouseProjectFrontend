a894f6e8c05f8d4e026df1b4d6910fa6767c17c7
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-12-02 01:02:55 +0600

    SUN-88 немного кастомизировала визивиг,доработала верстку

--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -5,18 +5,24 @@ import './Lesson.scss'
 import AddContentBlock from '../AddContentBlock/AddContentBlock'
 
 const Lesson = () => (
-  <div>
-    <div className="lesson-block">
-      <h1>Первый урок</h1>
-      <p>Содержимое занятия</p>
-      <SunEditorWYSIWYG />
-      <FilesUploader type="video" />
-      <FilesUploader type="audio" />
-      <AddContentBlock />
-      <div className="lesson-block_files">
-        <p className="lesson-block_files-title">Прикреплённые файлы</p>
-        <FilesUploader />
+  <div className="lesson-container container">
+    <div className="lesson-module">Блок добавления модуля</div>
+    <div className="lesson-blank">
+      <div className="lesson-block">
+        <h1 className="lesson-block_title">Первый урок</h1>
+        <div className="lesson-block_editor">
+          <p className="lesson-block_editor_title">Содержимое занятия</p>
+          <SunEditorWYSIWYG />
+        </div>
+        <FilesUploader type="video" />
+        <FilesUploader type="audio" />
+        <AddContentBlock />
+        <div className="lesson-block_files">
+          <p className="lesson-block_files_title">Прикреплённые файлы</p>
+          <FilesUploader />
+        </div>
       </div>
+      <button className="MainButton GreenButton lesson-save-button">Сохранить</button>
     </div>
   </div>
 )
