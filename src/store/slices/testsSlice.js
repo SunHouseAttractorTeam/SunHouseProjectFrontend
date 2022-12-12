0fa593d7008af2ac0c1af1a0c8e07f19312ed2bc
Erkin_Suyumbaev <smbv.erkin@gmail.com>
2022-12-12 13:45:33 +0600

    SUN-83 Добавил экшены в testsSlice

--- src/store/slices/testsSlice.js
+++ src/store/slices/testsSlice.js
@@ -12,6 +12,18 @@ const testsSlice = createSlice({
   name,
   initialState,
   reducers: {
+    fetchTestRequest(state) {
+      state.loading = true
+      state.error = null
+    },
+    fetchTestSuccess(state, { payload: test }) {
+      state.loading = false
+      state.test = test
+    },
+    fetchTestFailure(state, action) {
+      state.loading = false
+      state.error = action.payload
+    },
     createTestRequest(state) {
       state.loading = true
       state.error = null
@@ -20,6 +32,28 @@ const testsSlice = createSlice({
       state.loading = false
     },
     createTestFailure(state, action) {
+      state.loading = false
+      state.error = action.payload
+    },
+    editTestRequest(state) {
+      state.loading = true
+      state.error = null
+    },
+    editTestSuccess(state) {
+      state.loading = false
+    },
+    editTestFailure(state, action) {
+      state.loading = false
+      state.error = action.payload
+    },
+    deleteTestRequest(state) {
+      state.loading = true
+      state.error = null
+    },
+    deleteTestSuccess(state) {
+      state.loading = false
+    },
+    deleteTestFailure(state, action) {
       state.loading = false
       state.error = action.payload
     },
