a8bed04893eabd5324e9b2f41db6676fb169450f
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-12-12 21:19:59 +0600

    SUN-91 верстка сделана

---
+++ src/components/TaskBlock/TaskBlock.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContentForm from '../ContentForm/ContentForm'
+
+const TaskBlock = () => (
+  <div className="content-form ">
+    <ContentForm title="Задание" type="test" subtitle="Содержимое задания" />
+  </div>
+)
+
+export default TaskBlock
