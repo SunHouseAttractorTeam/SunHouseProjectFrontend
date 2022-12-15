b40dca24f884a44ba558544e9914f47b311ab763
anna.somik <kduzkdkuzkdkd@gmail.com>
2022-12-15 23:08:58 +0600

    EDU-13 добавила компонент "Course Program",верстка сделана на 90%

---
+++ src/components/CourseProgram/CourseProgram.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Paragraph from '../Paragraph/Paragraph'
+import './CourseProgram.scss'
+import CardProgram from './CardProgram/CardProgram'
+
+const CourseProgram = () => (
+  <div className="program-block">
+    <Paragraph title="Программа курса" subtitle="добавьте описании если необходимо" />
+    <div className="program-block__cards">
+      <CardProgram />
+      <CardProgram />
+      <CardProgram />
+      <CardProgram />
+      <button className="program-block__btn MainButton GreenButton">Записаться на курс</button>
+    </div>
+  </div>
+)
+
+export default CourseProgram
