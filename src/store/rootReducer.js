a48d89cfd48544ff214d218d34b104c14fd18b8d
Ramazan <r.tokoshev@gmail.com>
2022-12-15 15:03:06 +0600

    EDU-6 создал store для уведомлений

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,12 +3,14 @@ import usersSlice from './slices/usersSlices'
 import coursesSlice from './slices/coursesSlice'
 import categoriesSlice from './slices/categoriesSlice'
 import lessonsSlice from './slices/lessonsSlice'
+import notificationsSlice from './slices/notificationsSlice'
 
 const rootReducer = combineReducers({
   users: usersSlice.reducer,
   courses: coursesSlice.reducer,
   categories: categoriesSlice.reducer,
   lessons: lessonsSlice.reducer,
+  notifications: notificationsSlice.reducer,
 })
 
 export default rootReducer
---
+++ src/store/sagas/notificationSagas.js
@@ -0,0 +1,78 @@
+import { put, takeEvery } from 'redux-saga/effects'
+import axiosApi from '../../axiosApi'
+import {
+  createNotificationFailure,
+  createNotificationRequest,
+  createNotificationSuccess,
+  deleteNotificationFailure,
+  deleteNotificationRequest,
+  deleteNotificationSuccess,
+  editNotificationFailure,
+  editNotificationRequest,
+  editNotificationSuccess,
+  fetchNotificationFailure,
+  fetchNotificationRequest,
+  fetchNotificationsFailure,
+  fetchNotificationsRequest,
+  fetchNotificationsSuccess,
+  fetchNotificationSuccess,
+} from '../actions/notificationsActions'
+
+export function* fetchNotifications() {
+  try {
+    const response = yield axiosApi(`/notifications`)
+    yield put(fetchNotificationsSuccess(response.data))
+  } catch (e) {
+    yield put(fetchNotificationsFailure(e))
+  }
+}
+
+export function* fetchNotification({ payload: id }) {
+  try {
+    const response = yield axiosApi(`/notifications/${id}`)
+    yield put(fetchNotificationSuccess(response.data))
+  } catch (e) {
+    yield put(fetchNotificationFailure(e))
+  }
+}
+
+export function* createNotification({ payload: notificationData }) {
+  try {
+    yield axiosApi.post(`/notifications`, notificationData)
+    yield put(createNotificationSuccess())
+  } catch (e) {
+    yield put(createNotificationFailure(e))
+  }
+}
+
+export function* editNotification({ payload }) {
+  const { notificationId, notificationData } = payload
+
+  try {
+    yield axiosApi.put(`/notifications/${notificationId}`, notificationData)
+    yield put(editNotificationSuccess())
+  } catch (e) {
+    yield put(editNotificationFailure(e))
+  }
+}
+
+export function* deleteNotification({ payload }) {
+  const { notificationId } = payload
+
+  try {
+    yield axiosApi.delete(`/notifications/${notificationId}`)
+    yield put(deleteNotificationSuccess())
+  } catch (e) {
+    yield put(deleteNotificationFailure(e))
+  }
+}
+
+const notificationSagas = [
+  takeEvery(fetchNotificationsRequest, fetchNotifications),
+  takeEvery(fetchNotificationRequest, fetchNotification),
+  takeEvery(createNotificationRequest, createNotification),
+  takeEvery(editNotificationRequest, editNotification),
+  takeEvery(deleteNotificationRequest, deleteNotification),
+]
+
+export default notificationSagas
