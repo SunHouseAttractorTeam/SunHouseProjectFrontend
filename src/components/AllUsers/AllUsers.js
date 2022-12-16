55d69147acbcd27a371d04283b3732a948066a19
Atomico592 <atomico592@gmail.com>
2022-12-16 20:21:55 +0600

    EDU-26 Создал роуты для админской панели

---
+++ src/components/AllUsers/AllUsers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './AllUsers.scss'
+import Title from '../UI/Title/Title'
+
+const AllUsers = () => (
+  <div className="users">
+    <Title>Список пользователей</Title>
+  </div>
+)
+
+export default AllUsers
---
+++ src/components/LendingReviews/LendingReviews.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Title from '../UI/Title/Title'
+
+const LendingReviews = () => (
+  <div>
+    <Title>Отзывы</Title>
+  </div>
+)
+
+export default LendingReviews
