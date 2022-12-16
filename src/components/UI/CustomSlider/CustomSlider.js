624a505c03322efad8ce1674fe55c3748a808ac6
Kylymbek Shaihidinov <shaihidinovkylymbek@gmail.com>
2022-12-16 17:31:08 +0600

    EDU-24 Адаптивка лендинга

--- src/components/UI/CustomSlider/CustomSlider.js
+++ src/components/UI/CustomSlider/CustomSlider.js
@@ -28,6 +28,33 @@ function SamplePrevArrow(props) {
 
 const CustomSlider = ({ children }) => {
   const settings = {
+    responsive: [
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '150px',
+        },
+      },
+      {
+        breakpoint: 870,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+          centerPadding: '50px',
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
     infinite: false,
     slidesToShow: 2,
     slidesToScroll: 1,
