816d3ce8c0239c04d4e3e678ebabdfacf1b6e4f5
Atomico592 <atomico592@gmail.com>
2022-12-17 20:29:42 +0600

    EDU-26 добавил запрос на список пользователей, добавил иконки к роутам

--- src/components/AllUsers/AllUsers.js
+++ src/components/AllUsers/AllUsers.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './AllUsers.scss'
+import { useDispatch, useSelector } from 'react-redux'
 import Title from '../UI/Title/Title'
+import { getAllUsersRequest } from '../../store/actions/usersActions'
 
-const AllUsers = () => (
-  <div className="users">
-    <Title>Список пользователей</Title>
-  </div>
-)
+const AllUsers = () => {
+  const dispatch = useDispatch()
+  const users = useSelector(state => state.users.users)
+  useEffect(() => {
+    dispatch(getAllUsersRequest())
+  }, [dispatch])
+  console.log(users)
+  return (
+    <div className="users">
+      <Title>Список пользователей</Title>
+      <div className="users__inner-block">
+        <h5 className="users__subtitle">Имя пользователя</h5>
+        {users?.map(user => (
+          <div key={user._id}>
+            <p className="users__names">{user.username}</p>
+            <hr className="users__underline" />
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
 
 export default AllUsers
