4c309a517b6fb8f8a34b822c1a946acfcd2f0f85
Zanokha Olga <o.zanokha@gmail.com>
2022-12-20 16:16:15 +0600

    EDU-16 Сделала базовую логику - создание вопросов, ответов, удаление, редактирование

--- src/components/TestBlock/TestBlock.js
+++ src/components/TestBlock/TestBlock.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './TestBlock.scss'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
+import { fetchTestRequest } from '../../store/actions/testsActions'
 import ContentForm from '../ContentForm/ContentForm'
-import { editTestRequest, fetchTestRequest } from '../../store/actions/testsActions'
 import QuestionsBlock from '../QuestionsBlock/QuestionsBlock'
 
 const Test = () => {
@@ -11,12 +11,19 @@ const Test = () => {
   const dispatch = useDispatch()
   const test = useSelector(state => state.tests.test)
 
+  const [questionsState, setQuestionsState] = useState([{ title: '', answers: [{ title: '', status: false }] }])
+
   useEffect(() => {
     dispatch(fetchTestRequest(testId))
   }, [dispatch, testId])
 
   const handleSaveTest = data => {
-    dispatch(editTestRequest(data))
+    console.log(data)
+    // dispatch(editTestRequest(data))
+  }
+
+  const addQuestion = () => {
+    setQuestionsState(prev => [...prev, { title: '', answers: [{ title: '', status: false }] }])
   }
 
   return (
@@ -24,7 +31,14 @@ const Test = () => {
       {test && (
         <>
           <ContentForm contentData={test} contentId={testId} handleSave={handleSaveTest} />
-          <QuestionsBlock />
+          {questionsState.map((q, index) => (
+            <div key={index}>
+              <QuestionsBlock question={q} indexQuestion={index} setQuestion={setQuestionsState} />
+            </div>
+          ))}
+          <button type="button" onClick={addQuestion} className="question-block__add-button MainButton">
+            + Добавить вопрос
+          </button>
         </>
       )}
     </>
