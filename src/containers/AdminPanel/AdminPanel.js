7ec27ab867174dca7d02b219807b5197f8fd1366
Atomico592 <atomico592@gmail.com>
2022-12-22 15:06:13 +0600

    EDU-26 Написал таблицу, прописал стили, косо коряво но работает

--- src/containers/AdminPanel/AdminPanel.js
+++ src/containers/AdminPanel/AdminPanel.js
@@ -8,7 +8,7 @@ import { ProtectedRoute } from '../../utils/utils'
 import AllUsers from '../../components/AllUsers/AllUsers'
 import GetNotification from '../../components/GetNotification/GetNotification'
 import LendingReviews from '../../components/LendingReviews/LendingReviews'
-import LendingCourses from '../../components/LendingCourses/LendingCourses'
+import AdminCoursesControl from '../../components/AdminCoursesControl/AdminCoursesControl'
 
 const AdminPanel = () => {
   const user = useSelector(state => state.users.user)
@@ -38,7 +38,7 @@ const AdminPanel = () => {
                 isAllowed={Cookies.get('jwt')}
                 redirectTo="/login"
                 path="/admin_panel/courses"
-                component={LendingCourses}
+                component={AdminCoursesControl}
               />
               <ProtectedRoute
                 isAllowed={Cookies.get('jwt')}
