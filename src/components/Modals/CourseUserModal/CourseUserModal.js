927f5726a25fe6e8179c40fa41a13e763c493796
Zanokha Olga <o.zanokha@gmail.com>
2023-01-10 19:23:57 +0600

    EDU-8 Вывела курс в модалке

--- src/components/Modals/CourseUserModal/CourseUserModal.js
+++ src/components/Modals/CourseUserModal/CourseUserModal.js
@@ -1,36 +1,81 @@
 import React, { useEffect, useState } from 'react'
 import './CourseUserModal.scss'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Modal from '../../UI/Modal2/Modal'
 import Avatar from '../../UI/Avatar/Avatar'
 import { getUserRequest } from '../../../store/actions/coursesActions'
+import avatarStub from '../../../assets/icons/avatarStub.svg'
+import { apiUrl } from '../../../config'
+import MainButton from '../../UI/MainButton/MainButton'
 
 const CourseUserModal = ({ setOpen, email }) => {
   const dispatch = useDispatch()
-  const [course, setCourse] = useState({
-    title: '',
-    category: '',
-  })
+  const course = useSelector(state => state.courses.course)
+  let avatarImage = avatarStub
+  const [showMore, setShowMore] = useState(false)
 
   useEffect(() => {
     dispatch(getUserRequest(email))
-    console.log(email)
   }, [email, dispatch])
 
   const handlerClick = e => {
     setOpen(false)
   }
+  const onShowMoreBtn = () => {
+    setShowMore(!showMore)
+  }
 
+  if (course.image && course.image !== 'undefined') {
+    console.log(course.image)
+    avatarImage = `${apiUrl}/${course.image}`
+  }
   return (
-    <div className="course-user-modal__backdrop" onClick={handlerClick}>
+    <>
+      <div className="course-user-modal__backdrop" onClick={handlerClick} />
       <Modal setOpen={setOpen}>
         <div className="course-user-modal__header">
-          <Avatar className="course-user-modal__avatar" user={null} />
-          <h2 className="course-user-modal__title">email</h2>
-          <p className="course-user-modal__text">{email}</p>
+          <Avatar className="course-user-modal__header__avatar" user={null} />
+          <h2 className="course-user-modal__header__title">email</h2>
+          <p className="course-user-modal__header__text">{email}</p>
         </div>
+        <div className="container">
+          <div className="course-user-modal__title__inner">
+            <div className="course-user-modal__title__left">
+              <div>
+                <div className="course-user-modal__title__left-image">
+                  <img src={avatarImage} alt={course.title} />
+                </div>
+              </div>
+              <div className="course-user-modal__title__left-info">
+                <h2 className="course-user-modal__title__left-info-title">{course.title}</h2>
+              </div>
+            </div>
+            <MainButton
+              className="WhiteButton"
+              type="button"
+              onClick={onShowMoreBtn}
+              text={
+                <>
+                  <i>
+                    <svg width="16" height="10" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M13.1733 1.38686L7.99997 6.56019L2.82664 1.38686C2.30664 0.866856 1.46664 0.866856 0.946641 1.38686C0.426641 1.90686 0.426641 2.74686 0.946641 3.26686L7.06664 9.38686C7.58664 9.90686 8.42664 9.90686 8.94664 9.38686L15.0666 3.26686C15.5866 2.74686 15.5866 1.90686 15.0666 1.38686C14.5466 0.880189 13.6933 0.866856 13.1733 1.38686Z"
+                        fill="#1C1C1E"
+                      />
+                    </svg>
+                  </i>
+                </>
+              }
+            />
+          </div>
+        </div>
+        {showMore && (
+          <div className="course-user-modal__tests">
+            <h2 className="course-user-modal__test__title">Программа курса</h2>
+          </div>
+        )}
       </Modal>
-    </div>
+    </>
   )
 }
 
