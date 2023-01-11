2583c8331c60b9086eb539eac76aa89a55ed74bd
Vitaliy SCHeglov <mlacvital@gmail.com>
2023-01-11 23:07:26 +0600

    EDU-51 Применил компонент ButtonsContent ко всем блокам, уведомления и настройка профиля.

--- src/components/UI/ButtonsContent/ButtonsContent.js
+++ src/components/UI/ButtonsContent/ButtonsContent.js
@@ -1,25 +1,30 @@
 import React, { useState } from 'react'
 import './ButtonsContent.scss'
 
-const ButtonsContent = ({ titleOne, titleTwo, childrenOne, childrenTwo }) => {
+const ButtonsContent = ({ titleOne, titleTwo, childrenOne, childrenTwo, onClickOne, onClickTwo }) => {
   const [active, setActive] = useState(true)
+
   return (
     <div className="title-block">
       <div className="title-block__buttons">
-        <button
-          type="button"
-          onClick={() => setActive(true)}
-          className={active ? 'title-block__buttons_button activeClass' : 'title-block__buttons_button'}
-        >
-          {titleOne}
-        </button>
-        <button
-          type="button"
-          onClick={() => setActive(false)}
-          className={active ? 'title-block__buttons_button' : 'title-block__buttons_button activeClass'}
-        >
-          {titleTwo}
-        </button>
+        <div onClick={onClickOne}>
+          <button
+            type="button"
+            onClick={() => setActive(true)}
+            className={active ? 'title-block__buttons_button activeClass' : 'title-block__buttons_button'}
+          >
+            {titleOne}
+          </button>
+        </div>
+        <div onClick={onClickTwo}>
+          <button
+            type="button"
+            onClick={() => setActive(false)}
+            className={active ? 'title-block__buttons_button' : 'title-block__buttons_button activeClass'}
+          >
+            {titleTwo}
+          </button>
+        </div>
       </div>
       <div>{active ? <>{childrenOne}</> : <>{childrenTwo}</>}</div>
     </div>
