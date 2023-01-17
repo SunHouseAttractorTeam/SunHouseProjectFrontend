ade42c1224c2f516261f44ad2a9cdf3bb4d947d9
Atomico592 <atomico592@gmail.com>
2023-01-17 19:12:59 +0600

    EDU-59 частично исправил п***ц

--- src/components/LendingTeachersBlock/LendingTeachersBlock.js
+++ src/components/LendingTeachersBlock/LendingTeachersBlock.js
@@ -1,33 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CustomSlider from '../UI/CustomSlider/CustomSlider'
+import LendingTeacherCard from './LendingTecherCard/LendingTeacherCard'
+import { fetchTeachersRequest } from '../../store/actions/lendingTeachersActions'
+import './LendingTeacherBlock.scss'
 
-const sliderSettings = [
-  {
-    breakpoint: 1170,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: '150px',
-    },
-  },
-  {
-    breakpoint: 870,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: '50px',
-    },
-  },
-  {
-    breakpoint: 700,
-    settings: {
-      slidesToShow: 1,
-      slidesToScroll: 1,
-    },
-  },
-]
+const LendingTeacherBlock = () => {
+  const dispatch = useDispatch()
+  const teachers = useSelector(state => state.teachers.teachers)
 
-const LendingTeachersBlock = () => <div />
+  useEffect(() => {
+    dispatch(fetchTeachersRequest())
+  }, [dispatch])
+  console.log(teachers)
 
-export default LendingTeachersBlock
+  return (
+    <>
+      <div className="teachers_block">
+        <div className="container">
+          <div className="teachers_block_headline">
+            <div className="teachers_block_text">
+              <h5 className="teachers_block_title">Преподаватели — практикующие эксперты</h5>
+              <span className="teachers_block_subtitle">Доверьте свое обучение специалистам</span>
+            </div>
+          </div>
+          <CustomSlider>
+            <LendingTeacherCard
+              // image={teacher_1}
+              name="Александр Гаврилин"
+              description="Руководитель правовой практики в сфере ПО, технологий, сделок с брендом и данными ЯНДЕКС"
+            />
+          </CustomSlider>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LendingTeacherBlock
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -2,26 +2,26 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import About from '../../components/About/About'
-import TeachersBlock from '../../components/TeachersBlock/TeachersBlock'
 import Promo from '../../components/Promo/Promo'
 import CoursesCatalog from '../../components/CoursesCatalog/CoursesCatalog'
 import Partners from '../../components/Partners/Partners'
 import './Main.scss'
 import ReviewsBlock from '../../components/ReviewsBlock/ReviewsBlock'
-import { teachers } from '../../data/teachers'
+import LendingTeachersBlock from '../../components/LendingTeachersBlock/LendingTeachersBlock'
 
 const Main = () => (
   <div className="main">
     <Header />
     <Promo />
+    <LendingTeachersBlock />
     <CoursesCatalog />
     <About />
     <div className="container">
-      <TeachersBlock
-        title="Преподаватели — <span>практикующие эксперты</span>"
-        subtitle="Доверьте свое обучение специалистам"
-        teachers={teachers}
-      />
+      {/* <TeachersBlock */}
+      {/*  title="Преподаватели — <span>практикующие эксперты</span>" */}
+      {/*  subtitle="Доверьте свое обучение специалистам" */}
+      {/*  teachers={teachers} */}
+      {/* /> */}
     </div>
     <Partners />
     <ReviewsBlock />
