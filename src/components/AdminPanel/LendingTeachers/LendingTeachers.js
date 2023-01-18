ed8cd8e86587001c9cbf10ac68f673841d43c348
Atomico592 <atomico592@gmail.com>
2023-01-18 18:51:41 +0600

    EDU-59 Предпросмотр картинки

--- src/components/AdminPanel/LendingTeachers/LendingTeachers.js
+++ src/components/AdminPanel/LendingTeachers/LendingTeachers.js
@@ -12,16 +12,19 @@ import {
 } from '../../../store/actions/lendingTeachersActions'
 import { CreateNewObject } from '../../../utils/utils'
 import TeacherCard from '../../TeachersBlock/TeacherCard/TeacherCard'
+import { apiUrl } from '../../../config'
 
 const LendingTeachers = () => {
   const dispatch = useDispatch()
   const teachers = useSelector(state => state.teachers.teachers)
   const [state, setState] = useState({ image: '', name: '', description: '' })
+  const [image, setImage] = useState(null)
   const newTeachers = CreateNewObject(teachers)
   useEffect(() => {
     dispatch(fetchTeachersRequest())
   }, [dispatch])
   const fileChangeHandler = e => {
+    setImage(URL.createObjectURL(e.target.files[0]))
     const { name } = e.target
     const file = e.target.files[0]
     setState(prev => ({ ...prev, [name]: file }))
@@ -35,12 +38,14 @@ const LendingTeachers = () => {
     })
     dispatch(createTeachersRequest(formData))
     setState({ image: '', name: '', description: '' })
+    setImage(null)
   }
   const deleteTeacherForLending = id => {
     dispatch(deleteTeachersRequest(id))
   }
+
   return (
-    <div className="pupa">
+    <>
       <div className="reviews">
         <Title>Преподаватели</Title>
         <div className="reviews__wrapper">
@@ -48,15 +53,23 @@ const LendingTeachers = () => {
           <div className="reviews__wrapper-inner">
             <div className="reviews__wrapper-inner2">
               <div className="reviews__input-label">
+                <img
+                  src={image || (state.image ? `${apiUrl}/${state.image}` : null)}
+                  alt={state.title}
+                  className="reviews__input-label-image"
+                />
+                {!state.image ? (
+                  <i className="reviews__input-img">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M24 21.3333V2.66667C24 1.2 22.8 0 21.3333 0H2.66667C1.2 0 0 1.2 0 2.66667V21.3333C0 22.8 1.2 24 2.66667 24H21.3333C22.8 24 24 22.8 24 21.3333ZM7.86667 14.64L10.6667 18.0133L14.8 12.6933C15.0667 12.3467 15.6 12.3467 15.8667 12.7067L20.5467 18.9467C20.88 19.3867 20.56 20.0133 20.0133 20.0133H4.02667C3.46667 20.0133 3.16 19.3733 3.50667 18.9333L6.82667 14.6667C7.08 14.32 7.58667 14.3067 7.86667 14.64Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </i>
+                ) : null}
+
                 <input type="file" name="image" className="reviews__input-file" onChange={fileChangeHandler} />
-                <i className="reviews__input-img">
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M24 21.3333V2.66667C24 1.2 22.8 0 21.3333 0H2.66667C1.2 0 0 1.2 0 2.66667V21.3333C0 22.8 1.2 24 2.66667 24H21.3333C22.8 24 24 22.8 24 21.3333ZM7.86667 14.64L10.6667 18.0133L14.8 12.6933C15.0667 12.3467 15.6 12.3467 15.8667 12.7067L20.5467 18.9467C20.88 19.3867 20.56 20.0133 20.0133 20.0133H4.02667C3.46667 20.0133 3.16 19.3733 3.50667 18.9333L6.82667 14.6667C7.08 14.32 7.58667 14.3067 7.86667 14.64Z"
-                      fill="white"
-                    />
-                  </svg>
-                </i>
               </div>
               <div className="reviews__wrapper-inner3">
                 <FormInput
@@ -116,7 +129,7 @@ const LendingTeachers = () => {
           ))}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
