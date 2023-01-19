682feaa87b0c65656d8dc2ee0674d82597d08993
Atomico592 <atomico592@gmail.com>
2023-01-19 21:25:39 +0600

    EDU-59 Добавил удаление отзывов и защиту от отправки пустых данных

--- src/components/AdminPanel/LendingReviews/LendingReviews.js
+++ src/components/AdminPanel/LendingReviews/LendingReviews.js
@@ -19,7 +19,9 @@ const LendingReviews = () => {
   const dispatch = useDispatch()
   const [state, setState] = useState({ image: '', name: '', socialNetwork: '', description: '' })
   const reviews = useSelector(state => state.reviews.reviews)
+  const [image, setImage] = useState(null)
   const fileChangeHandler = e => {
+    setImage(URL.createObjectURL(e.target.files[0]))
     const { name } = e.target
     const file = e.target.files[0]
     setState(prev => ({ ...prev, [name]: file }))
@@ -37,9 +39,9 @@ const LendingReviews = () => {
     })
     dispatch(createReviewRequest(formData))
     setState({ image: '', name: '', socialNetwork: '', description: '' })
+    setImage(null)
   }
   const deleteReviewCard = id => {
-    console.log(id)
     dispatch(deleteReviewRequest(id))
   }
   return (
@@ -51,15 +53,22 @@ const LendingReviews = () => {
           <div className="reviews__wrapper-inner">
             <div className="reviews__wrapper-inner2">
               <div className="reviews__input-label">
+                <img
+                  src={image || (state.image ? `${apiUrl}/${state.image}` : null)}
+                  alt={state.title}
+                  className="reviews__input-label-image"
+                />
                 <input type="file" name="image" className="reviews__input-file" onChange={fileChangeHandler} />
-                <i className="reviews__input-img">
-                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M24 21.3333V2.66667C24 1.2 22.8 0 21.3333 0H2.66667C1.2 0 0 1.2 0 2.66667V21.3333C0 22.8 1.2 24 2.66667 24H21.3333C22.8 24 24 22.8 24 21.3333ZM7.86667 14.64L10.6667 18.0133L14.8 12.6933C15.0667 12.3467 15.6 12.3467 15.8667 12.7067L20.5467 18.9467C20.88 19.3867 20.56 20.0133 20.0133 20.0133H4.02667C3.46667 20.0133 3.16 19.3733 3.50667 18.9333L6.82667 14.6667C7.08 14.32 7.58667 14.3067 7.86667 14.64Z"
-                      fill="white"
-                    />
-                  </svg>
-                </i>
+                {!state.image ? (
+                  <i className="reviews__input-img">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M24 21.3333V2.66667C24 1.2 22.8 0 21.3333 0H2.66667C1.2 0 0 1.2 0 2.66667V21.3333C0 22.8 1.2 24 2.66667 24H21.3333C22.8 24 24 22.8 24 21.3333ZM7.86667 14.64L10.6667 18.0133L14.8 12.6933C15.0667 12.3467 15.6 12.3467 15.8667 12.7067L20.5467 18.9467C20.88 19.3867 20.56 20.0133 20.0133 20.0133H4.02667C3.46667 20.0133 3.16 19.3733 3.50667 18.9333L6.82667 14.6667C7.08 14.32 7.58667 14.3067 7.86667 14.64Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </i>
+                ) : null}
               </div>
               <div className="reviews__wrapper-inner3">
                 <FormInput
@@ -104,10 +113,15 @@ const LendingReviews = () => {
           </div>
         </div>
         <MainButton
+          disabled={!state.name || !state.image || !state.description}
           text="Добавить"
           onClick={e => submitFormHandler(e)}
           type="submit"
-          className="GreenButton reviews__button"
+          className={
+            state.name && state.description && state.image && state.socialNetwork
+              ? 'GreenButton reviews__button'
+              : 'GreenButton reviews__button reviews__button-disabled'
+          }
         />
       </div>
 
