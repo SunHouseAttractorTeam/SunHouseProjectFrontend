6d1e88f0f79015289871f234268a2e7a85e8a27d
Atomico592 <atomico592@gmail.com>
2023-01-23 20:43:49 +0600

    Решил конфликты

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export let apiUrl = 'http://localhost:8000'
+export const apiUrl = 'http://localhost:8000'
 export const FacebookAppId = process.env.REACT_APP_FACEBOOK_APP_ID
 export const GoogleAppId = process.env.REACT_APP_GOOGLE_APP_ID
 export const VKAppId = process.env.REACT_APP_VK_APP_ID
