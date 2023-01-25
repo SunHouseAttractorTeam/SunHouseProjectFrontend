087a286fa0ab0449f144d93a35aaadbeb692aeba
Vitaliy SCHeglov <mlacvital@gmail.com>
2023-01-25 20:31:05 +0600

    EDU-69 Добавил логику на отправку данных в google-sheet для добавления данных в таблицу.

--- src/store/sagas/usersSagas.js
+++ src/store/sagas/usersSagas.js
@@ -44,6 +44,7 @@ import {
   verifyUserSuccess,
 } from '../actions/usersActions'
 import { fetchCourseRequest } from '../actions/coursesActions'
+import { sendGFFailure, sendGFRequest, sendGFSuccess } from '../actions/sendGFActions'
 
 const Toast = Swal.mixin({
   toast: true,
@@ -244,8 +245,6 @@ export function* editUserPasswordSaga({ payload: passwords }) {
 export function* checkUserTask({ payload: data }) {
   try {
     yield put(showLoading())
-
-    console.log(data)
     yield axiosApi.patch(
       `/users/${data.userId}/update_status?content=${data.taskId}&params=task&course=${data.courseId}&choice=${data.value}`,
     )
@@ -278,6 +277,28 @@ export function* updateUserContentStatusSaga({ payload: { userId, content, path 
   }
 }
 
+export function* sendGoogleFormSaga({ payload }) {
+  try {
+    yield put(showLoading())
+    const response = yield axiosApi.post('https://sheet.best/api/sheets/8a0fe7b6-7041-4649-a1fe-0e28f49780e4', payload)
+    yield put(sendGFSuccess(response.data))
+    yield Swal.fire({
+      toast: false,
+      icon: 'success',
+      title: `Сообщение отправлено!`,
+    })
+    yield put(hideLoading())
+  } catch (e) {
+    if (e.response && e.response.data) {
+      yield put(sendGFFailure(e.response.data))
+      yield Toast.fire({
+        icon: 'error',
+        title: 'Сообщение не отправлено!',
+      })
+    }
+  }
+}
+
 const userSagas = [
   takeEvery(loginUserRequest, loginUserSaga),
   takeEvery(banUnbanRequest, banUnbanSaga),
@@ -292,6 +313,7 @@ const userSagas = [
   takeEvery(passwordRequest, editUserPasswordSaga),
   takeEvery(checkUserTaskRequest, checkUserTask),
   takeEvery(updateUserContentStatusRequest, updateUserContentStatusSaga),
+  takeEvery(sendGFRequest, sendGoogleFormSaga),
 ]
 
 export default userSagas
