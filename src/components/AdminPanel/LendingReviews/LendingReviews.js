bd699a4fcb981f458e14b55ce58940d5e56bbcba
aman <amanmaksat0@gmail.com>
2023-01-28 22:18:50 +0600

    EDU-76 исправил прохождение, добавил цену при создании курса.

--- src/components/AdminPanel/LendingReviews/LendingReviews.js
+++ src/components/AdminPanel/LendingReviews/LendingReviews.js
@@ -131,7 +131,7 @@ const LendingReviews = () => {
           {reviews.map(review => (
             <span key={review._id} className="teacher-inner-block__content-card">
               <ReviewCard
-                img={review.image ? `${apiUrl}/${review.image}` : noPhoto}
+                img={review.image ? `${apiUrl}/uploads/${review.image}` : noPhoto}
                 name={review.name}
                 social={review.socialNetwork}
                 content={review.description}
