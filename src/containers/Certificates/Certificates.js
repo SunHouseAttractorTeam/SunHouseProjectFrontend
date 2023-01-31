1dadcc09d517f0454e1c94fb7e552611acd583cc
Zanokha Olga <o.zanokha@gmail.com>
2023-01-31 18:06:04 +0600

    Адаптивность, лого, логика

--- src/containers/Certificates/Certificates.js
+++ src/containers/Certificates/Certificates.js
@@ -10,22 +10,28 @@ const CERTIFICATES = [
             'Обучающие материалы были разработаны при поддержке ' +
             'Кыргызско-германской-швейцарской программы "Зеленая экономика и учтойчивое развитие частного сектора в КР".'
     },
-    // {
-    //     course: 'UX-UI',
-    //     username: "Виталий Анатольевич",
-    //     datetime: '24 октября 2022 года',
-    // },
+    {
+        course: 'UI-UX', username: 'Виталий Анатольевич', description: 'В рамках проекта "Зеленая экономика ' +
+            'и устойчивое развитие частного сектора в КР". ' +
+            'Обучающие материалы были разработаны при поддержке ' +
+            'Кыргызско-германской-швейцарской программы "Зеленая экономика и учтойчивое развитие частного сектора в КР".'
+    },
 ]
 
 const Certificates = () => (
     <div className="certificates">
         <Title>Мои сертификаты</Title>
         <div className="certificates__block">
+
             {CERTIFICATES.map((certificate, i) => (
                 // eslint-disable-next-line react/no-array-index-key
-                <Certificate key={certificate.course + i} course={certificate.course} fullName={certificate.username}
-                             description={certificate.description}/>
+                <div className={"certificates__block__certificate"}>
+                    <Certificate key={certificate.course + i} course={certificate.course}
+                                 fullName={certificate.username}
+                                 description={certificate.description}/>
+                </div>
             ))}
+
         </div>
     </div>
 )
